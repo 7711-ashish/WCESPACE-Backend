a98d2045364e8d167bcf4566a42f2ef517145961
7711-ashish <ashishamolsutar0302@gmail.com>
2023-05-22 16:44:22 +0530

    Auth added in history section

--- server/controllers/key/getKeys.js
+++ server/controllers/key/getKeys.js
@@ -31,7 +31,7 @@ const getKeys = async (req, res) => {
     const { name } = req.query;
 
     if (!name) {
-      const keys = await requests.find({'resources.list':{$in:["Class19"]}});
+      const keys = await requests.find({'resources.list':{$in:[name]}});
       if (!keys) throw "History not found";
       console.log(keys)
 
