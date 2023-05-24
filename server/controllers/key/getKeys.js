214385f601981d3a993074a51c251e191836bc26
7711-ashish <ashishamolsutar0302@gmail.com>
2023-05-24 09:07:50 +0530

    Changes in keyhistory backend

--- server/controllers/key/getKeys.js
+++ server/controllers/key/getKeys.js
@@ -1,47 +1,18 @@
-import keysSchema from "../../models/keys.js";
 import requests from "../../models/requests.js";
-import resources from "../../models/resources.js";
 import {
   sendFailResponse,
   sendSuccessResponse,
 } from "../../utils/responses.js";
 
-// const getKeys = async (req, res) => {
-//   try {
-//     const { id } = req.query;
-
-//     if (!id) {
-//       const keys = await keysSchema.find({ department: req.user.department });
-//       if (!keys) throw "Keys not found";
-
-//       sendSuccessResponse({ res, data: keys });
-//     } else {
-//       const key = await keysSchema.findById(id);
-//       if (!key) throw "Key not found";
-
-//       sendSuccessResponse({ res, data: key });
-//     }
-//   } catch (err) {
-//     sendFailResponse({ res, statusCode: 400, err });
-//   }
-// };
-
 const getKeys = async (req, res) => {
 
   try {
     const  name  = req.params.name;
-
-    if (!name) {
-      const keys = await  await requests.find({'resources.list':{$in:[name]}, status : "approved"});
+    console.log(name)
+      const keys = await requests.find({'resources.list':{$in:[name]}});
       if (!keys) throw "History not found";
 
       sendSuccessResponse({ res, data: keys });
-    } else {
-      const key = await keysSchema.findById(id);
-      if (!key) throw "History not found";
-
-      sendSuccessResponse({ res, data: key });
-    }
   } catch (err) {
     sendFailResponse({ res, statusCode: 400, err });
   }
