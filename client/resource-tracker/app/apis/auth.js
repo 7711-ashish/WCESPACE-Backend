d407cd1e07e7ae82e2ad1cad4b681fa45e0a0119
kajalp23 <kajalpawar2382001@gmail.com>
2023-05-24 22:39:57 +0530

    Add password validation

--- client/resource-tracker/app/apis/auth.js
+++ client/resource-tracker/app/apis/auth.js
@@ -1,10 +1,27 @@
 import apiClient from "./client";
+import { View, TextInput, Button, Alert } from 'react-native';
+
 
 const signIn = (payload) => {
     return apiClient.post("/users/signin", payload);
 };
 
 const signUp = (payload) => {
+    console.log(payload.password)
+    const strongPasswordPattern = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+    if (!strongPasswordPattern.test(payload.password)) {
+      Alert.alert(
+        'Error',
+        'Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one digit, and one special character.'
+      );
+      return {
+        res : {
+            ok: false,
+            data: ""
+        }
+      };
+    }
     return apiClient.post("/users/signup", payload);
 };
 
